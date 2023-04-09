bff1a03ed2eec378b0976f96a9d58907ae622ad5
matan922 <matanten@gmail.com>
2023-04-09 19:47:55 +0300

    added receiver for to init stats of new user

--- backend/clicker_game/models.py
+++ backend/clicker_game/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 # Create your models here.
 
@@ -8,3 +10,13 @@ class ClickerDetails(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null = True)
     coins = models.CharField(max_length=255)
     workers = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.user.username
+    
+    
+@receiver(post_save, sender=User)
+def init_user_stats(sender, instance, created, **kwargs):
+    if created:
+        stats = ClickerDetails(coins="0", workers="0", user=instance)
+        stats.save()
